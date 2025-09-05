e4a697bda68df3d19ec7c85fcfa1392ce939bec6
Hemank <betterartyish24@gmail.com>
2025-09-05 12:56:36 +0530

    Doctor Sign Up and Sign In

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Login from './components/Login'
 import MainLogo from './components/MainLogo';
 import SignUp from './components/SignUp';
+import DoctorSignUp from './components/DoctorSignUp';
+import DoctorLogin from './components/DoctorLogin';
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/doctorsignup" element={<DoctorSignUp />} />
+        <Route path="/doctorlogin" element={<DoctorLogin />} />
       </Routes>
     </BrowserRouter>
   )
